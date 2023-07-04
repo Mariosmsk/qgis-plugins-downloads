77ca75cb60b49b5a47a55ffd12445acf9b3a5c45
Marios S. Kyriakou <mariosmsk@gmail.com>
2023-07-05 00:18:25 +0300

    First commit

---
+++ qgis-plugin-downloads.py
@@ -0,0 +1,45 @@
+# !/usr/bin/python
+"""
+***********************************************************************************
+Name                 : qgis-plugin-downloads
+Description          : Download the information about qgis plugins.
+Date                 : 5/July/2023
+copyright            : (C) 2023 by Marios S. Kyriakou, University of Cyprus,
+                       KIOS Research and Innovation Center of Excellence (KIOS CoE)
+email                : mariosmsk@gmail.com
+***********************************************************************************
+"""
+
+import requests
+import pandas as pd
+import time
+import json
+import os
+
+num_pages = 3  # number of pages
+all_tables = []
+for i in range(1, num_pages):
+    print(i)
+    url = f"https://plugins.qgis.org/plugins/?page={i}&sort=name&"
+
+    response = requests.get(url)
+    if response.status_code != 200:
+        break
+
+    tables = pd.read_html(response.content)
+    tables = [table.drop(table.columns[0], axis=1) for table in tables]
+    table = tables[0][pd.to_numeric(tables[0]['Downloads'], errors="coerce").notna()]
+    all_tables.append(table)
+    time.sleep(0.5)
+
+combined_table = pd.concat(all_tables, ignore_index=True)
+
+os.makedirs("jsons", exist_ok=True)
+
+for index, row in combined_table.iterrows():
+    data = row.to_dict()
+    filename = f"jsons/{data[list(data.keys())[0]]}.json"
+    with open(filename, "w") as json_file:
+        json.dump(data, json_file)
+
+combined_table.to_csv("qgis-plugins-downloads.csv", index=False)
