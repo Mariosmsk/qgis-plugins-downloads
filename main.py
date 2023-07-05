ab4749910c5664de45ec318d47bfff1f0d551861
Marios S. Kyriakou <mariosmsk@gmail.com>
2023-07-05 13:24:17 +0300

    newst version to update daily the folder data with files: `plugins.json`, `plugins.csv`, `plugins_time_series.csv`

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ copyright            : (C) 2023 by Marios S. Kyriakou, University of Cyprus,
 email                : mariosmsk@gmail.com
 ***********************************************************************************
 """
-
+import datetime
 import requests
 import pandas as pd
 import time
 import json
 import os
 
-num_pages = 150  # number of pages
+num_pages = 150 # number of pages
+
+# Read all plugin information
 all_tables = []
 for i in range(1, num_pages):
     url = f"https://plugins.qgis.org/plugins/?page={i}&sort=name&"
-
     response = requests.get(url)
     if response.status_code != 200:
         break
@@ -32,17 +33,78 @@ for i in range(1, num_pages):
     all_tables.append(table)
     time.sleep(0.5)
 
+# Get current date
+current_datetime = datetime.datetime.now().date()
+datetime_string = current_datetime.strftime("%Y-%m-%d")
 combined_table = pd.concat(all_tables, ignore_index=True)
 
-os.makedirs("jsons", exist_ok=True)
+# Create if not exists the folder `data`
+os.makedirs("data", exist_ok=True)
 
+# Create lists with plugin information
+plugin_data = []
+plugin_data_ts = []
 for index, row in combined_table.iterrows():
     data = row.to_dict()
     plugin_name = data[list(data.keys())[0]]
     plugin_name = plugin_name.replace('/', '_')
     plugin_name = plugin_name.replace(' ', '_')
-    filename = f"jsons/{plugin_name}.json"
-    with open(filename, "w") as json_file:
-        json.dump(data, json_file)
+    plugin_download = row['Downloads']
+    author = data['Author']
+    latest_update = data['Latest Plugin Version']
+    stars = data['Stars (votes)']
+    stars = stars.replace('(', '')
+    stars = stars.replace(')', '')
+    pl = {
+        "plugin_name": plugin_name,
+        "author": author,
+        "latest_update": latest_update,
+        "downloads": plugin_download,
+        "stars": stars,
+        "date": datetime_string
+    }
+    plugin_data.append(pl)
+    pl_ts = {
+        "plugin_name": plugin_name,
+        "downloads": plugin_download,
+        "date": datetime_string
+    }
+    plugin_data_ts.append(pl_ts)
 
-combined_table.to_csv("qgis-plugins-downloads.csv", index=False)
+# Create the `plugins.json` file / the latest information
+filename_plugins = f"data/plugins.json"
+with open(filename_plugins, "w") as json_file:
+    json.dump(plugin_data, json_file)
+
+# Create the `plugins.csv` file / the latest information
+df = pd.DataFrame(plugin_data)
+df.to_csv("data/plugins.csv", index=False)
+
+# Make the date columns
+df_ts = pd.DataFrame(plugin_data_ts)
+df_pivot = df.pivot(index="plugin_name", columns="date", values="downloads")
+
+filename_ts_plugins = f"data/plugins_time_series.csv"
+if not os.path.exists(filename_ts_plugins):
+    df_pivot.to_csv(filename_ts_plugins)
+
+existing_data = pd.read_csv(filename_ts_plugins)
+
+# Add the missing plugins in the file
+last_plugins = set(df['plugin_name'])
+existing_plugins = set(existing_data['plugin_name'])
+missing_plugins = []
+if last_plugins == existing_plugins:
+    pass
+else:
+    missing_plugins = last_plugins - existing_plugins
+
+for plugin in missing_plugins:
+    index = df['plugin_name'].tolist().index(plugin)
+    new_row = {"plugin_name": plugin, f"{datetime_string}": df['downloads'][index]}
+    existing_data = pd.concat([existing_data, pd.DataFrame([new_row])], ignore_index=True)
+
+# Update the file `plugins_time_series.csv`
+merged_df = pd.merge(existing_data, df_pivot, on='plugin_name', how='inner')
+merged_df.to_csv(filename_ts_plugins, index=False)
+
